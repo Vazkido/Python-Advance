ead0ab2635991833e53501f3f2f14bba2a1bf571
vazkido <vazkid02@gmail.com>
2026-01-10 03:24:53 -0600

    UPDATE

---
+++ GIT/intro.py
@@ -0,0 +1,14 @@
+# 10th January 2026
+#git-scm.
+
+name = input("Enter Your Name: ")
+#version 1
+print("Hello World")
+
+"""#version 2
+print("Hello", Name)
+
+#version 3
+print(f"Hello {Name}")
+"""
+
